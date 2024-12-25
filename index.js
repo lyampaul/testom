cfc50d2dc349ab1d05655f3f92c3c10ed86103fd
lyampaul <lyampaul@outlook.com>
2024-12-26 01:57:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const browser = await puppeteer.launch();
 const page = await browser.newPage();
 await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.0 Safari/537.36");
 await page.setViewport({ width: 1280, height: 720 });
-await page.goto(`https://tinyurl.com/andrewlyam`);
+//await page.goto(`https://tinyurl.com/andrewlyam`);
+await page.goto(`https://nicekid.pages.dev/`)
 console.log(`Taking screenshot`)
 await page.screenshot({ path:`./latest.png`, fullPage: true });
 setTimeout(() => process.exit(), 3600000)
