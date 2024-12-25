79d62beed7b9ef3c73425ba5566b384187510338
lyampaul <lyampaul@outlook.com>
2024-12-26 01:16:21 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const puppeteer = require('puppeteer');
+
+(async() => {
+const browser = await puppeteer.launch();
+
+const page = await browser.newPage();
+await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.0 Safari/537.36");
+await page.setViewport({ width: 1280, height: 720 });
+await page.goto(`https://tinyurl.com/andrewlyam`);
+console.log(`Taking screenshot`)
+await page.screenshot({ path:`./latest.png`, fullPage: true });
+setTimeout(() => process.exit(), 120000)
+})();
